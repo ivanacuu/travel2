4f27c2a077c410257701ed71fe196f516fdb1cf6
ivanacuu <i.curcic@emakina.rs>
2021-07-13 17:40:48 +0200

    first commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,23 @@
+var slideIndex = 1;
+var end = false;
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+
+  if (n < 1) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  slides[slideIndex - 1].classList.remove("second");
+}
